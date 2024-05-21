b29f41229188e5bddb993761f59cefaf6ffb0a38
Dmitriy <xs_ds@inbox.ru>
2024-05-21 17:58:11 +0500

    register tag for categories

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Configure Debug Toolbar is shown only localhost
