ee60f2f5905a4582a11736de122417749ff9328f
Dmitriy <xs_ds@inbox.ru>
2024-05-27 14:34:23 +0500

    pagination added 1

--- goods/urls.py
+++ goods/urls.py
@@ -5,6 +5,7 @@ from goods.apps import GoodsConfig
 app_name = GoodsConfig.name
 
 urlpatterns = [
-    path('', views.catalog, name='index'),
+    path('<slug:category_slug>/', views.catalog, name='index'),
+    path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
 from goods.models import Products
+from django.core.paginator import Paginator
 
 
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug, page=1):
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
+
     context = {
         'title': 'Home - Каталог',
-        'goods': goods,
+        'goods': current_page,
+        'slug_url': category_slug,
     }
     return render(request, 'goods/catalog.html', context)
 
