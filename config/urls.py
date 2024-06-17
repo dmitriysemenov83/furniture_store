e4fe344775516c8b58db702da6108c948b7ab3d2
Dmitriy <xs_ds@inbox.ru>
2024-06-17 16:41:23 +0500

    cart application and templates added

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog')),
     path('user/', include('users.urls', namespace='user')),
+    path('cart/', include('carts.urls', namespace='cart')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if DEBUG:
