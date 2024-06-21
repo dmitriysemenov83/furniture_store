fcfa4a2ada263bac0e2110bbb947087a3830f00e
Dmitriy <xs_ds@inbox.ru>
2024-06-21 13:06:09 +0500

    Add to cart view, refactor included cart templates

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from goods.models import Products
+from users.models import User
+
+from goods.models import NULLABLE
+
+
+class CartQueryset(models.QuerySet):
+    def total_price(self):
+        return sum(cart.product_price() for cart in self)
+
+    def total_quantity(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        return 0
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE, verbose_name='Пользователь')
+    session_key = models.CharField(max_length=32, **NULLABLE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, verbose_name='Товар')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    class Meta:
+        db_table = 'cart'
+        verbose_name = 'Корзину'
+        verbose_name_plural = 'Корзины'
+
+    objects = CartQueryset().as_manager()
+
+    def product_price(self):
+        return round(self.product.sell_price() * self.quantity, 2)
+
+    def __str__(self):
+        return f'Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}'
